50c067e9b0c6b20283842dec695b00ac21630aef
AlinaPavlova25 <alina.26.pav@gmail.com>
2026-03-07 16:26:49 +0300

    fix: bos veritabaninda seed otomatik calistir

--- init_db.py
+++ init_db.py
@@ -1,6 +1,10 @@
 from app import app, db
 from flask_migrate import stamp
+from models import Brand
 
 with app.app_context():
     db.create_all()
     stamp()
+    if Brand.query.count() == 0:
+        from seed import seed
+        seed()
